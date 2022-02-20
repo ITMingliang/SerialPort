5b9fcce6722a52393d1665a1603a4bc744042333
mingliang.wang <mingliang_xyz@outlook.com>
2022-02-20 22:22:06 +0800

    init add

---
+++ 01.串口通信/01.串口通信/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace _01.串口通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.tbTranbmit = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox5);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(33, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 421);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口通信连接";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(38, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口号：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(100, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(38, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "校验位：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(38, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "存储位：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(38, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "波特率：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(38, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "停止位：";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "115200",
+            "500000"});
+            this.comboBox2.Location = new System.Drawing.Point(100, 103);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(206, 20);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "9600";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "8",
+            "7",
+            "6"});
+            this.comboBox3.Location = new System.Drawing.Point(100, 162);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(206, 20);
+            this.comboBox3.TabIndex = 7;
+            this.comboBox3.Text = "8";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even",
+            "Mark",
+            "Space"});
+            this.comboBox4.Location = new System.Drawing.Point(100, 221);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(206, 20);
+            this.comboBox4.TabIndex = 8;
+            this.comboBox4.Text = "None";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "1.5",
+            "2"});
+            this.comboBox5.Location = new System.Drawing.Point(100, 283);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(206, 20);
+            this.comboBox5.TabIndex = 9;
+            this.comboBox5.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(63, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "连接通讯";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(206, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "扫描端口";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbTranbmit
+            // 
+            this.tbTranbmit.Location = new System.Drawing.Point(42, 481);
+            this.tbTranbmit.Multiline = true;
+            this.tbTranbmit.Name = "tbTranbmit";
+            this.tbTranbmit.Size = new System.Drawing.Size(605, 70);
+            this.tbTranbmit.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(665, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "发送";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(501, 39);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(287, 421);
+            this.rtbResult.TabIndex = 13;
+            this.rtbResult.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 563);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tbTranbmit);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox tbTranbmit;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 01.串口通信/01.串口通信/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01.串口通信
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = -1;
+            comboBox1.Items.Clear();
+            string [] port =SerialPort.GetPortNames();
+            Array.Sort(port);
+            comboBox1.Items.AddRange(port);
+            comboBox1.SelectedIndex = comboBox1.Items.Count > 0 ? 0 : -1;
+
+        }
+
+        SerialPort spl =new SerialPort();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (spl.IsOpen==false)
+            {
+                spl.PortName=comboBox1.Text;
+                spl.BaudRate=Convert.ToInt32(comboBox2.Text);
+                spl.DataBits=Convert.ToInt32(comboBox3.Text);
+
+                //校验位
+                if (comboBox4.Text=="None")
+                    spl.Parity = Parity.None;
+                if (comboBox4.Text == "Even")
+                    spl.Parity = Parity.Even;
+                if (comboBox4.Text == "Odd")
+                    spl.Parity = Parity.Odd;
+                if (comboBox4.Text == "Mark")
+                    spl.Parity = Parity.Mark;
+                if (comboBox4.Text == "Space")
+                    spl.Parity = Parity.Space;
+
+                //停止位
+                if (comboBox5.Text == "0")
+                    spl.StopBits = StopBits.None;
+                if (comboBox5.Text == "1")
+                    spl.StopBits = StopBits.One;
+                if (comboBox5.Text == "1.5")
+                    spl.StopBits = StopBits.OnePointFive;
+                if (comboBox5.Text == "2")
+                    spl.StopBits = StopBits.Two;
+
+
+                try
+                {
+                    spl.Open();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message,"错误",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    return; 
+                }
+                button1.Text = "关闭串口";
+
+            }
+            else
+            {
+                try
+                {
+                    spl.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                button1.Text = "打开串口";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (spl.IsOpen)
+            {
+                string sendMessage = tbTranbmit.Text;
+                spl.Write(sendMessage);
+                rtbResult.Text += "\r\n" + "发送:" + sendMessage;
+            }
+            else
+            {
+                MessageBox.Show("串口未连接!");
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (spl.IsOpen)
+            {
+                Byte[] receveData = new Byte[spl.BytesToRead];
+                spl.Read(receveData, 0, receveData.Length);
+                spl.DiscardInBuffer();
+                string strRev = null;
+
+                //接收数据
+                if (receveData.Length > 0)
+                {
+                    strRev += Encoding.Default.GetString(receveData);
+                    rtbResult.Text += "\r\n" + "接收:" + strRev;
+                }
+            }
+        }
+    }
+}
